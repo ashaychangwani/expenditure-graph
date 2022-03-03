912153072b095aa51879af0da593745fee9ec515
Ashay Changwani <ashaychangwani@gmail.com>
2022-03-04 05:16:34 +0530

    Transactions logic added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Transactions from "./components/Transactions";
+import Expense from "./classes/expense";
+import Names from "./components/Names";
+
+
 function App() {
+  const [allNames, setAllNames] = useState([]);
+
+  const handleOpenForm = () => {
+    console.log("Form opened");
+    setFlag(!false);
+  };
   return (
     <div className="App">
       <header className="App-header">
